1aae89d198058daedcc25c171f296977e9c91d7e
Andres Rueda <garo99@gmail.com>
2020-09-04 10:43:04 -0400

    Router

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,30 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+import ArtistRoute from '../ArtistRoute';
+
+import GlobalStyles from '../GlobalStyles';
+
+const DEFAULT_ARTIST_ID = '44TGR1CzjKBxSHsSEy7bi9';
 
 const App = () => {
-  return <Router>TODO</Router>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/artist/:id">
+          <ArtistRoute />
+        </Route>
+        <Route path="/">
+          <Redirect to={`/artist/${DEFAULT_ARTIST_ID}`} />
+        </Route>
+      </Switch>
+      <GlobalStyles />
+    </Router>
+  );
 };
 
 export default App;
---
+++ src/components/ArtistRoute.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function ArtistRoute() {
+  return <div>HELLO ARTIST!!!!</div>;
+}
