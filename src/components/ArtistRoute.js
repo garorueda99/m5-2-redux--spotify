263446cceb14f9d165d3ff8238ed9caa3a343f83
Andres Rueda <garo99@gmail.com>
2020-09-07 23:18:00 -0400

    style

--- src/components/ArtistRoute.js
+++ src/components/ArtistRoute.js
@@ -4,6 +4,8 @@ import { fetchArtistProfile } from '../helpers/api-helpers';
 import { useParams } from 'react-router-dom';
 import { receiveArtistInfo, receiveArtistInfoError } from '../actions';
 import { useDispatch } from 'react-redux';
+import styled from 'styled-components';
+import { COLORS } from '../theme';
 
 export default function ArtistRoute() {
   const dispatch = useDispatch();
@@ -22,14 +24,14 @@ export default function ArtistRoute() {
   return (
     <>
       {artist !== null ? (
-        <div>
+        <Wrapper>
           <img src={artist.images[0].url} alt={`${artist.name} images`} />
           <h1>{artist.name}</h1>
           <p>{format(artist.followers.total)}</p>
           {artist.genres.slice(0, 2).map((item, index) => (
             <div key={index}>{item}</div>
           ))}
-        </div>
+        </Wrapper>
       ) : (
         'LOADING'
       )}
@@ -37,11 +39,23 @@ export default function ArtistRoute() {
   );
 }
 
+const Wrapper = styled.div`
+  background-color: ${COLORS.charcoal};
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: ${COLORS.white};
+`;
+
 const format = (follower) => {
   switch (true) {
     case follower / 1000 < 999:
       return `${Math.round(follower / 1000)}K`;
     case follower / 1000000 < 999:
       return `${Math.round(follower / 1000000)}M`;
+    default:
+      return follower;
   }
 };
---
+++ src/theme.js
@@ -0,0 +1,8 @@
+export const COLORS = {
+  primary: '#FF4FD8',
+  secondary: '#3354FF',
+  white: '#FFFFFF',
+  silver: '#797F86',
+  charcoal: '#0B0F14',
+  grayfade: 'rgba(75, 75, 75, 0.4);',
+};
